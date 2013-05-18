ce114f26efe3999d9d2f3378f20204abed5a7ef8
Jesse Duarte <jesseduarte13@gmail.com>
2013-05-18 16:45:40 -0700

    Implemented Cellar

--- src/com/_500bottles/object/cellar/Cellar.java
+++ src/com/_500bottles/object/cellar/Cellar.java
@@ -1,5 +1,39 @@
 package com._500bottles.object.cellar;
 
-public class Cellar {
+import java.util.Vector;
+
+public class Cellar
+{
+	private Vector<CellarItem> collection;
+
+	public Cellar()
+	{
+		collection = new Vector<CellarItem>();
+	}
+
+	public Vector<CellarItem> getCollection()
+	{
+		return collection;
+	}
+
+	public void add(CellarItem c)
+	{
+		collection.addElement(c);
+	}
+
+	public boolean contains(CellarItem c)
+	{
+		return collection.contains(c);
+	}
+
+	public int size()
+	{
+		return collection.size();
+	}
+
+	public boolean remove(CellarItem c)
+	{
+		return collection.remove(c);
+	}
 
 }
