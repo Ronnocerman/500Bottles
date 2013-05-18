db85171c8fc7b5870ea31d607f870dc25fd5a5af
yunchoi <choi.yunsung@gmail.com>
2013-05-18 13:55:04 -0700

    - Implemented photo object

--- src/com/_500bottles/object/winebook/Photo.java
+++ src/com/_500bottles/object/winebook/Photo.java
@@ -1,4 +1,63 @@
 package com._500bottles.object.winebook;
 
-public class Photo {
+public class Photo
+{
+
+	private long photoId;
+	private String filename;
+
+	/**
+	 * 
+	 */
+	public Photo()
+	{
+
+	}
+
+	/**
+	 * 
+	 * @param photoId
+	 * @param filename
+	 */
+	public Photo(long photoId, String filename)
+	{
+		this.setPhotoId(photoId);
+		this.setFilename(filename);
+	}
+
+	/**
+	 * 
+	 * @return
+	 */
+	public long getPhotoId()
+	{
+		return photoId;
+	}
+
+	/**
+	 * 
+	 * @param photoId
+	 */
+	public void setPhotoId(long photoId)
+	{
+		this.photoId = photoId;
+	}
+
+	/**
+	 * 
+	 * @return
+	 */
+	public String getFilename()
+	{
+		return filename;
+	}
+
+	/**
+	 * 
+	 * @param filename
+	 */
+	public void setFilename(String filename)
+	{
+		this.filename = filename;
+	}
 }
