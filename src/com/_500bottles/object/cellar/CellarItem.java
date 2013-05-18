1b55e2fee99dbc8230a5a6ed2585a8b7f2cb9f95
Jesse Duarte <jesseduarte13@gmail.com>
2013-05-18 16:18:51 -0700

    Implemented CellarItemId

--- src/com/_500bottles/object/cellar/CellarItem.java
+++ src/com/_500bottles/object/cellar/CellarItem.java
@@ -6,4 +6,52 @@ public class CellarItem
 	private int quantity;
 	private String notes;
 	private long wineId;
+
+	public int getCellarItemId()
+	{
+		return cellarItemId;
+	}
+
+	public void setCellarItemId(int cellarItemId)
+	{
+		this.cellarItemId = cellarItemId;
+	}
+
+	public int getQuantity()
+	{
+		return quantity;
+	}
+
+	public void setQuantity(int quantity)
+	{
+		this.quantity = quantity;
+	}
+
+	public String getNotes()
+	{
+		return notes;
+	}
+
+	public void setNotes(String notes)
+	{
+		this.notes = notes;
+	}
+
+	public long getWineId()
+	{
+		return wineId;
+	}
+
+	public void setWineId(long wineId)
+	{
+		this.wineId = wineId;
+	}
+
+	@Override
+	public boolean equals(Object o)
+	{
+		CellarItem c = (CellarItem) o;
+		return new String(Integer.toString(this.getCellarItemId()))
+				.equals(new String(Integer.toString(c.getCellarItemId())));
+	}
 }
