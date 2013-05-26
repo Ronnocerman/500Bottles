d6319abd21ce690a075517477c9692407b818252
Joshua Toenyes <josh@117degrees.com>
2013-05-25 17:20:16 -0700

    - Updated NSFWException and WineException to include standard   exception default constructors. - Set NSFWException to extend standard Java Exception.

--- src/com/_500bottles/exception/NSFWException.java
+++ src/com/_500bottles/exception/NSFWException.java
@@ -1,5 +1,19 @@
 package com._500bottles.exception;
 
-public class NSFWException {
+public class NSFWException extends Exception
+{
+	public NSFWException()
+	{
+		super();
+	}
 
+	public NSFWException(String msg)
+	{
+		super(msg);
+	}
+
+	public NSFWException(String msg, Throwable cause)
+	{
+		super(msg, cause);
+	}
 }
