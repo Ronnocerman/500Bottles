8d69641ea0bc8f8fb00fb1cb3365d3a43e793c28
Joshua Toenyes <josh@117degrees.com>
2013-05-25 17:23:42 -0700

    - Added DAException class.

---
+++ src/com/_500bottles/exception/da/DAException.java
@@ -0,0 +1,28 @@
+package com._500bottles.exception.da;
+
+import com._500bottles.exception.NSFWException;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: administrator
+ * Date: 5/25/13
+ * Time: 3:31 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class DAException extends NSFWException
+{
+	public DAException()
+	{
+		super();
+	}
+
+	public DAException(String msg)
+	{
+		super(msg);
+	}
+
+	public DAException(String msg, Throwable cause)
+	{
+		super(msg, cause);
+	}
+}
