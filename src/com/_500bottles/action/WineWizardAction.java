4e13adff3351db034f1a775c04cc583b3cb74528
Martin La Pierre <mlboldeagle@gmail.com>
2013-05-26 19:39:47 -0700

    added in some comments and things for the action class to set in the manager level depending on what the user wants

--- src/com/_500bottles/action/WineWizardAction.java
+++ src/com/_500bottles/action/WineWizardAction.java
@@ -11,7 +11,10 @@ public class WineWizardAction
 	// advance
 	// search
 	// level one
-	private String wineName;
+	private boolean fromCellar;// a flag to see if user wants wine from their
+								// cellar
+	private boolean fromOnline;// a flag to see if user wants wine from online
+	private String wineName;// a string of the wine name the user wants
 	private String appellation;// set for advanced search level five
 	private String year;// set for advanced search level three
 	private String varietal;// set for advanced search level two
@@ -21,10 +24,14 @@ public class WineWizardAction
 	private double sensitiveYear;// the weight of the year
 	private double sensitiveVineyard;// the weight of the vineyard
 	private double sensitiveAppellation;// the weight of the appellation
-	private int wineAmount;
-	private int counter;
-	private ArrayList<Wine> returnWine = new ArrayList<Wine>();
+	private int wineAmount;// the amount that the user wants
+	private int counter;// counter used to keep track of the current wine to
+						// display
+	private ArrayList<Wine> returnWine = new ArrayList<Wine>();// the list of
+																// wines to
+																// return
 
+	// returns wines by parsing a list
 	public Wine getWines()
 	{
 		if (counter < returnWine.size())
@@ -35,10 +42,70 @@ public class WineWizardAction
 		return null;
 	}
 
+	public WineWizardAction()
+	{
+		sensitiveType = -1;// the weight of the type
+		sensitiveVarietal = -1;
+		sensitiveYear = -1;
+		sensitiveVineyard = -1;
+		sensitiveAppellation = -1;
+	}
+
+	// gets the wines suggestion based on if user traits have been set
 	public void getSuggestion()
 	{
+		// sets counter to zero
 		counter = 0;
+		// creates new WineWizardManager
 		WineWizardManager wizardManager = new WineWizardManager(wineAmount);
+		if (fromCellar)
+		{
+			wizardManager.setFromCellar(true);
+		}
+		if (fromOnline)
+		{
+			wizardManager.setFromOnline(true);
+		}
+		if (sensitiveAppellation != -1)
+		{
+			wizardManager.setSensitiveAppellation(sensitiveAppellation);
+		}
+		if (sensitiveYear != -1)
+		{
+			wizardManager.setSensitiveYear(sensitiveYear);
+		}
+		if (sensitiveVineyard != -1)
+		{
+			wizardManager.setSensitiveVineyard(sensitiveVineyard);
+		}
+		if (sensitiveVarietal != -1)
+		{
+			wizardManager.setSensitiveVarietal(sensitiveVarietal);
+		}
+		if (sensitiveType != -1)
+		{
+			wizardManager.setSensitiveType(sensitiveType);
+		}
+		if (wineType != null)
+		{
+			wizardManager.setWineType(wineType);
+		}
+		if (appellation != null)
+		{
+			wizardManager.setAppellation(appellation);
+		}
+		if (year != null)
+		{
+			wizardManager.setYear(year);
+		}
+		if (varietal != null)
+		{
+			wizardManager.setVarietal(varietal);
+		}
+		if (vineyard != null)
+		{
+			wizardManager.setAppellation(vineyard);
+		}
 		if (wineName != null)
 		{
 			returnWine = wizardManager.findWine();
@@ -171,5 +238,25 @@ public class WineWizardAction
 	public void setWineName(String wineName)
 	{
 		this.wineName = wineName;
+	}
+
+	public boolean isFromCellar()
+	{
+		return fromCellar;
+	}
+
+	public void setFromCellar(boolean fromCellar)
+	{
+		this.fromCellar = fromCellar;
+	}
+
+	public boolean isFromOnline()
+	{
+		return fromOnline;
+	}
+
+	public void setFromOnline(boolean fromOnline)
+	{
+		this.fromOnline = fromOnline;
 	}
 }
