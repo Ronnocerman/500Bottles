27ab9a118556bdc19a93f634a1989cf291bd1d36
Joshua Toenyes <josh@117degrees.com>
2013-05-30 22:26:12 -0700

    - Added early-stage full bg login page. - More style updates.

--- WebContent/js/home.js
+++ WebContent/js/home.js
@@ -32,6 +32,83 @@
         });
     }
 
+    function sign_up_interaction()
+    {
+        var fn = document.getElementById("create_account_firstname");
+        var ln = document.getElementById("create_account_lastname");
+        var e = document.getElementById("create_account_email");
+        var p = document.getElementById("create_account_pwd");
+        var fp = document.getElementById("create_account_faux_pwd");
+        var cp = document.getElementById("create_account_conf_pwd");
+        var fcp = document.getElementById("create_account_faux_conf_pwd");
+        var fn_placeholder = fn.value;
+        var ln_placeholder = ln.value;
+        var e_placeholder = e.value;
+        var fp_placeholder = fp.value;
+        var fcp_placeholder = fcp.value;
+
+        fn.addEventListener("focus", function() {
+            if (fn.value == fn_placeholder)
+                fn.value = "";
+        });
+
+        fn.addEventListener("blur", function() {
+            if (fn.value == "")
+                fn.value = fn_placeholder;
+        });
+
+        ln.addEventListener("focus", function() {
+            if (ln.value == ln_placeholder)
+                ln.value = "";
+        });
+
+        ln.addEventListener("blur", function() {
+            if (ln.value == "")
+                ln.value = ln_placeholder;
+        });
+
+        e.addEventListener("focus", function() {
+            if (e.value == e_placeholder)
+                e.value = "";
+        });
+
+        e.addEventListener("blur", function() {
+            if (e.value == "")
+                e.value = e_placeholder;
+        });
+
+
+
+
+        fp.addEventListener("focus", function() {
+            fp.style.display = "none";
+            p.style.display = "block";
+            p.focus();
+        });
+
+        fcp.addEventListener("focus", function() {
+            fcp.style.display = "none";
+            cp.style.display = "block";
+            cp.focus();
+        });
+
+        p.addEventListener("blur", function() {
+            if (p.value != "")
+                return;
+
+            p.style.display = "none";
+            fp.style.display = "block";
+        });
+
+        cp.addEventListener("blur", function() {
+            if (cp.value != "")
+                return;
+
+            cp.style.display = "none";
+            fcp.style.display = "block";
+        });
+    }
+
     function login_view_interaction()
     {
         var login = document.getElementById("log_in");
@@ -62,13 +139,14 @@
         }
 
         login.addEventListener("click", show_login_view);
-        view_login.addEventListener("click", hide_login_view);
+        //view_login.addEventListener("click", hide_login_view);
     }
 
     function on_load()
     {
         login_form_interaction();
         login_view_interaction();
+        sign_up_interaction();
     }
 
     window.addEventListener("load", on_load);
