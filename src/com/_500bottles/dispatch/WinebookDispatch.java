6a671d00070238380cdfe2c54379459e7c4c20c5
ebandy <elisabandy@gmail.com>
2013-06-01 15:34:04 -0700

    - Added test_winebook.jsp - Modified some of the dispatch files for testing.

--- src/com/_500bottles/dispatch/WinebookDispatch.java
+++ src/com/_500bottles/dispatch/WinebookDispatch.java
@@ -14,7 +14,41 @@ public class WinebookDispatch extends HttpServlet
 	protected void doGet(HttpServletRequest request,
 			HttpServletResponse response) throws ServletException, IOException
 	{
-		// TODO Auto-generated method stub
+		String action = request.getParameter("action");
+
+		PrintWriter out = response.getWriter();
+
+		switch (action) {
+			case "addEntry":
+				out.println("addEntry clicked");
+				break;
+			case "getEntry":
+				out.println("getEntry clicked");
+				break;
+			case "removeEntry":
+				out.println("removeEntry clicked");
+				break;
+			case "editContent":
+				out.println("editContent clicked");
+				break;
+			case "addWine":
+				out.println("addWine clicked");
+				break;
+			case "removeWine":
+				out.println("removeWine clicked");
+				break;
+			case "addPhoto":
+				out.println("addPhoto clicked");
+				break;
+			case: "removePhoto":
+				out.println("removePhoto clicked");
+				break;
+			case: "uploadPhoto":
+				out.println("uploadPhoto clicked");
+				break;
+			default:
+				out.println("error");
+		}
 	}
 
 	@Override
