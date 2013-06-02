7f50da18d5e2135951244d5efeec9e70ca03be7c
Ronnocerman <Ronnocerman@yahoo.com>
2013-06-01 19:48:47 -0700

    Added header comments to UserAction and CellarAction.

--- src/com/_500bottles/action/CellarAction.java
+++ src/com/_500bottles/action/CellarAction.java
@@ -7,45 +7,111 @@ import com._500bottles.object.wine.Wine;
 
 public class CellarAction
 {
-
-	public void setCellarQuantity(long userID, Wine w, int q)
+	/**
+	 * Method to set the quantity of a particular wine in a user's cellar.
+	 * 
+	 * @param userID
+	 *            The userID of the user whose cellar is being edited.
+	 * @param wine
+	 *            The wine whose quantity to edit.
+	 * @param quantity
+	 *            The quantity to which to set the wine in the cellar.
+	 * @throws CellarException
+	 *             if the user does not exist.
+	 */
+	public void setCellarQuantity(long userID, Wine wine, int quantity)
 			throws CellarException
 	{
-		CellarItem ci = CellarManager.getByWineID(userID, w.getId());
-		ci.setQuantity(q);
+		CellarItem ci = CellarManager.getByWineID(userID, wine.getId());
+		ci.setQuantity(quantity);
 		CellarManager.editCellarItem(ci);
 	}
 
-	public void incCellarQuantity(long userID, Wine w) throws CellarException
+	/**
+	 * Increments the quantity of a particular wine in a user's cellar.
+	 * 
+	 * @param userID
+	 *            The userID of the user whose cellar is being incremented.
+	 * @param wine
+	 *            The wine whose quantity to increment.
+	 * @throws CellarException
+	 *             if the user does not exist.
+	 */
+	public void incCellarQuantity(long userID, Wine wine)
+			throws CellarException
 	{
-		CellarItem ci = CellarManager.getByWineID(userID, w.getId());
+		CellarItem ci = CellarManager.getByWineID(userID, wine.getId());
 		ci.setQuantity(ci.getQuantity() + 1);
 		CellarManager.editCellarItem(ci);
 	}
 
-	public void decCellarQuantity(long userID, Wine w) throws CellarException
+	/**
+	 * Decrements the quantity of a particular wine in a user's cellar.
+	 * 
+	 * @param userID
+	 *            The userID of the user whose cellar is being incremented
+	 * @param wine
+	 *            The wine whose quantity to increment.
+	 * @throws CellarException
+	 *             if the user does not exist.
+	 */
+	public void decCellarQuantity(long userID, Wine wine)
+			throws CellarException
 	{
-		CellarItem ci = CellarManager.getByWineID(userID, w.getId());
+		CellarItem ci = CellarManager.getByWineID(userID, wine.getId());
 		ci.setQuantity(ci.getQuantity() - 1);
 		CellarManager.editCellarItem(ci);
 	}
 
-	public void setCellarNotes(long userID, Wine w, String notes)
+	/**
+	 * Sets the cellar notes of a user for a particular wine.
+	 * 
+	 * @param userID
+	 *            The userID of the user for whom to add notes for a wine.
+	 * @param wine
+	 *            The wine for which to add notes.
+	 * @param notes
+	 *            The notes to add to the wine.
+	 * @throws CellarException
+	 *             if the user does not exist.
+	 */
+	public void setCellarNotes(long userID, Wine wine, String notes)
 			throws CellarException
 	{
-		CellarItem ci = CellarManager.getByWineID(userID, w.getId());
+		CellarItem ci = CellarManager.getByWineID(userID, wine.getId());
 		ci.setNotes(notes);
 		CellarManager.editCellarItem(ci);
 	}
 
-	public String getCellarNotes(long userID, Wine w) throws CellarException
+	/**
+	 * Gets the cellar notes of a user for a particular wine.
+	 * 
+	 * @param userID
+	 *            The userID of the user whose wine notes are being retrieved.
+	 * @param wine
+	 *            The wine for which to retrieve notes.
+	 * @return The notes for the wine by the user.
+	 * @throws CellarException
+	 *             if the user does not exist.
+	 */
+	public String getCellarNotes(long userID, Wine wine) throws CellarException
 	{
-		return CellarManager.getByWineID(userID, w.getId()).getNotes();
+		return CellarManager.getByWineID(userID, wine.getId()).getNotes();
 	}
 
-	public void clearCellarNotes(long userID, Wine w) throws CellarException
+	/**
+	 * Clears the cellar notes by a user for a particular wine.
+	 * 
+	 * @param userID
+	 *            The userID of the user whose notes to clear
+	 * @param wine
+	 *            The wine whose notes to clear.
+	 * @throws CellarException
+	 *             if the user does not exist.
+	 */
+	public void clearCellarNotes(long userID, Wine wine) throws CellarException
 	{
-		CellarItem ci = CellarManager.getByWineID(userID, w.getId());
+		CellarItem ci = CellarManager.getByWineID(userID, wine.getId());
 		ci.setNotes("");
 		CellarManager.editCellarItem(ci);
 	}
