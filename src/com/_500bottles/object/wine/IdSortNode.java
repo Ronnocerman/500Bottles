215cf420bee575e12fe829c175ee8c15dead84da
rcshaw <rcshaw@ucsd.edu>
2013-06-05 21:08:16 -0700

    The sorting classes that Marty wrote for our compare algorithm

---
+++ src/com/_500bottles/object/wine/IdSortNode.java
@@ -0,0 +1,29 @@
+package com._500bottles.object.wine;
+
+
+public class IdSortNode
+{
+	private int amount;
+	private long value;
+
+	public int getAmount()
+	{
+		return amount;
+	}
+
+	public void setAmount(int amount)
+	{
+		this.amount = amount;
+	}
+
+	public long getValue()
+	{
+		return value;
+	}
+
+	public void setValue(long value)
+	{
+		this.value = value;
+	}
+
+}
