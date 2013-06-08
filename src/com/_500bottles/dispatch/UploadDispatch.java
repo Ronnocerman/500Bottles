4c490820bd289bb97f74599da2c1388c86a0836b
merth <merth@128.54.98.28>
2013-06-08 16:07:25 -0700

    upload image, fixed winebook entry

--- src/com/_500bottles/dispatch/UploadDispatch.java
+++ src/com/_500bottles/dispatch/UploadDispatch.java
@@ -1,15 +1,7 @@
 package com._500bottles.dispatch;
 
-import org.apache.commons.fileupload.FileItem;
-import org.apache.commons.fileupload.FileUploadException;
-import org.apache.commons.fileupload.disk.DiskFileItemFactory;
-import org.apache.commons.fileupload.servlet.ServletFileUpload;
-import org.apache.commons.io.FilenameUtils;
-
-import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
-import java.io.OutputStream;
 import java.util.List;
 
 import javax.servlet.ServletException;
@@ -17,33 +9,60 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.apache.commons.fileupload.FileItem;
+import org.apache.commons.fileupload.FileUploadException;
+import org.apache.commons.fileupload.disk.DiskFileItemFactory;
+import org.apache.commons.fileupload.servlet.ServletFileUpload;
+import org.apache.commons.io.FilenameUtils;
+
+import com._500bottles.action.UploadAction;
+
 /**
  * Servlet implementation class NSFWServlet
  */
 public class UploadDispatch extends HttpServlet
 {
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		try {
-			List<FileItem> items = new ServletFileUpload(new DiskFileItemFactory()).parseRequest(request);
+	@Override
+	protected void doPost(HttpServletRequest request,
+			HttpServletResponse response) throws ServletException, IOException
+	{
+		try
+		{
+			List<FileItem> items = new ServletFileUpload(
+					new DiskFileItemFactory()).parseRequest(request);
 
-			for (FileItem item : items) {
-				if (item.isFormField()) {
-					// Process regular form field (input type="text|radio|checkbox|etc", select, etc).
+			for (FileItem item : items)
+			{
+				if (item.isFormField())
+				{
+					// Process regular form field (input
+					// type="text|radio|checkbox|etc", select, etc).
 					String fieldname = item.getFieldName();
 					String fieldvalue = item.getString();
-				// ... (do your job here)
-				} else {
-				// Process form file field (input type="file").
+
+					// ... (do your job here)
+				} else
+				{
+					// Process form file field (input type="file").
 					String fieldname = item.getFieldName();
 					String filename = FilenameUtils.getName(item.getName());
-					InputStream filecontent = item.getInputStream();
 
-					System.err.println("filename: " + filename);
+					if (filename != null && filename != "")
+					{
+						InputStream filecontent = item.getInputStream();
+
+						long id = UploadAction.addPhoto(item, filename
+								.substring(filename.lastIndexOf("."),
+										filename.length()));
+						System.out.println(id);
+					}
+
 				}
 			}
 
-		} catch (FileUploadException e) {
-			throw new ServletException("Cannot parse multipart request.", e);
+		} catch (FileUploadException e)
+		{
+			System.err.println("UploadDispatch: tried to upload image, failed");
 		}
 	}
 }
