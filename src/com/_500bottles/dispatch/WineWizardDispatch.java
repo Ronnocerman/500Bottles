2bd94a3852986decb5fcbc728baa74c0b697c66f
Martin La Pierre <mlboldeagle@gmail.com>
2013-06-07 22:07:48 -0700

    Marty's attempt at messing with the dispatch level prepare for excitement

--- src/com/_500bottles/dispatch/WineWizardDispatch.java
+++ src/com/_500bottles/dispatch/WineWizardDispatch.java
@@ -1,7 +1,10 @@
 package com._500bottles.dispatch;
 
 import java.io.IOException;
+import java.io.PrintWriter;
 
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -15,8 +18,30 @@ public class WineWizardDispatch extends HttpServlet
 			HttpServletResponse response) throws ServletException, IOException
 	{
 		// TODO Auto-generated method stub
+		String action = request.getParameter("action");
+		PrintWriter out = response.getWriter();
+		switch (action)
+		{
+		case "getSuggestion":
+			getSuggestion(request, response);
+		}
+
 	}
-	
+
+	private void getSuggestion(HttpServletRequest request,
+			HttpServletResponse response)
+	{
+		// TODO Auto-generated method stub
+
+		ServletContext context = getServletContext();
+
+		RequestDispatcher dispatcher = context
+				.getRequestDispatcher("/views/wizard.jsp");
+		String wineType = request.getParameter("checkbox");
+		String varietal = request.getParameter("varietalSelect");
+
+	}
+
 	@Override
 	protected void doPost(HttpServletRequest request,
 			HttpServletResponse response) throws ServletException, IOException
