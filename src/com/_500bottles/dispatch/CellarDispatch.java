6629c556eec61ad5e50499531f0bae4e04b9dd65
Jesse Duarte <jesseduarte13@gmail.com>
2013-06-08 20:43:01 -0700

    Fixed a bad thing sorry

--- src/com/_500bottles/dispatch/CellarDispatch.java
+++ src/com/_500bottles/dispatch/CellarDispatch.java
@@ -98,19 +98,15 @@ public class CellarDispatch extends HttpServlet
 		ServletContext context = getServletContext();
 
 		RequestDispatcher dispatcher = context
-				.getRequestDispatcher("/messages/js_callback.jsp");
+			.getRequestDispatcher("/messages/js_callback.jsp");
 
 		String id = request.getParameter(WINEID_FIELD);
 
 		CellarAction.incCellarQuantity(Long.parseLong(id));
 
-		CellarAction.incCellarQuantity(Long.parseLong(id));
-
-		try
-		{
+		try {
 			dispatcher.forward(request, response);
-		} catch (ServletException | IOException e)
-		{
+		} catch (ServletException | IOException e) {
 
 		}
 	}
@@ -121,23 +117,19 @@ public class CellarDispatch extends HttpServlet
 		ServletContext context = getServletContext();
 
 		RequestDispatcher dispatcher = context
-				.getRequestDispatcher("/messages/js_callback.jsp");
+			.getRequestDispatcher("/messages/js_callback.jsp");
 
 		String id = request.getParameter(WINEID_FIELD);
 
-		try
-		{
+		try {
 			CellarAction.decCellarQuantity(Long.parseLong(id));
-		} catch (CellarException e)
-		{
+		} catch (CellarException e) {
 			// TODO
 		}
 
-		try
-		{
+		try {
 			dispatcher.forward(request, response);
-		} catch (ServletException | IOException e)
-		{
+		} catch (ServletException | IOException e) {
 
 		}
 	}
