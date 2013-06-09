f9143c740ceee5d3c0930029fbeafe131f71131d
Joshua Toenyes <joshua.toenyes@me.com>
2013-06-09 01:13:17 -0700

    - Added drag-and-drop image uploading to winebook entry. - Added required js libraries for image uploading. - Cleaned up UploadDispatch.

--- WebContent/js/winebook.js
+++ WebContent/js/winebook.js
@@ -40,6 +40,7 @@
             return;
         }
 
+        setup_droppable();
         _500bottles.views.show_floating_view(entry_form);
     }
 
@@ -48,11 +49,58 @@
         _500bottles.views.hide_floating_view(entry_form);
     }
 
+
+    function setup_droppable()
+    {
+        var url = "/upload";
+
+        $('#fileupload').fileupload({
+            url: url,
+            dataType: 'json',
+            done: function (e, data) {
+                var imageId = data.result;
+            },
+            progressall: function (e, data) {
+                var progress = parseInt(data.loaded / data.total * 100, 10);
+                $('#progress .bar').css(
+                    'width',
+                    progress + '%'
+                );
+            }
+        });
+    }
+
+    function get_image_url(image_id)
+    {
+        var url = "/";
+
+        var data = {
+            "action": "getView",
+            "view": "winebook/entry_form"
+        };
+
+        $.ajax({
+            url: url,
+            data: data
+        }).success(function (data, textStatus, jqXHR) {
+                _500bottles.views.add_floating_view({
+                    contents: data,
+                    id: "winebook_entry_form"
+                });
+
+                entry_form = document.getElementById("winebook_entry_form");
+
+                $(entry_form).children(".close_icon").on("click", close_entry_form);
+
+                open_entry_form();
+            });
+    }
+
+
     function bind_events()
     {
         $(add_winebook_entry).on("click", open_entry_form);
     }
 
     bind_events();
-
 })();
--- WebContent/views/winebook/entry_form.jsp
+++ WebContent/views/winebook/entry_form.jsp
@@ -6,12 +6,15 @@
     <h2 id="winebook_entry_form_title">New Winebook Entry</h2>
 
     <form action="/upload" method="post" enctype="multipart/form-data">
-        <%--<input type="text" id="winebook_title" value="title" />--%>
-        <%--<textarea>today I made some liver and fava beans, so I decided to crack open a nice chianti to go with it...</textarea>--%>
-        <%--<input id="entry_save" type="button" value="save"/>--%>
-        <input type="text" name="filename" />
-        <input type="file" name="file" />
-        <input type="submit" />
+        <input type="text" id="winebook_title" value="title" />
+        <textarea>today I made some liver and fava beans, so I decided to crack open a nice chianti to go with it...</textarea>
+        <input id="entry_save" type="button" value="save"/>
     </form>
+
+    <input id="fileupload" type="file" name="files[]" multiple>
+
+    <div id="progress" class="progress progress-success progress-striped">
+        <div class="bar"></div>
+    </div>
 
 </section>
