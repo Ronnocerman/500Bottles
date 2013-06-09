7eda7e9370403ac79a856007692f866ea97a1130
Joshua Toenyes <joshua.toenyes@me.com>
2013-06-08 20:29:33 -0700

    - Renamed RatingsAction to uppercase R.

--- src/com/_500bottles/action/RatingsAction.java
+++ src/com/_500bottles/action/RatingsAction.java
@@ -1,27 +1,27 @@
 package com._500bottles.action;
 
-import com._500bottles.manager._RatingsManager;
+import com._500bottles.manager.RatingsManager;
 
 public class RatingsAction
 {
 	public static void addRating(long wineId, double rating)
 	{
-		_RatingsManager.addRating(wineId, rating);
+		RatingsManager.addRating(wineId, rating);
 	}
 
 	public static void removeRating(long wineId)
 	{
-		_RatingsManager.removeRating(wineId);
+		RatingsManager.removeRating(wineId);
 	}
 
 	public static void editRating(long wineId, double rating)
 	{
-		_RatingsManager.editRating(wineId, rating);
+		RatingsManager.editRating(wineId, rating);
 	}
 
 	public static double getRating(long wineId)
 	{
-		return _RatingsManager.getRating(wineId);
+		return RatingsManager.getRating(wineId);
 
 	}
 }
